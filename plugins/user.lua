b4c8fd10e41d8b3ecdabef1246514fab758707b8
梁焯辉 <liangchaohui@liangchaohuideMac-mini.local>
2024-02-03 15:07:57 +0800

    修改copilot默认配置

--- plugins/user.lua
+++ plugins/user.lua
@@ -17,7 +17,7 @@ return {
       require('copilot').setup({
         panel = {
           enabled = true,
-          auto_refresh = false,
+          auto_refresh = true,
           keymap = {
             jump_prev = "[[",
             jump_next = "]]",
@@ -26,13 +26,13 @@ return {
             open = "<M-CR>"
           },
           layout = {
-            position = "bottom", -- | top | left | right
+            position = "right", -- | top | left | right
             ratio = 0.4
           },
         },
         suggestion = {
           enabled = true,
-          auto_trigger = false,
+          auto_trigger = true,
           debounce = 75,
           keymap = {
             accept = "<M-l>",
